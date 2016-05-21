ee2ca3527be51e852c3a08d98360db7824ffa5e9
Shahab <safaeishahab@yahoo.com>
2016-05-21 11:51:47 +0430

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+
+
+fi
